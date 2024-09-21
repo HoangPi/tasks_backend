a8798de680e6c88511be94b655dc32e308db5161
Phi <voduchoangphi@gmail.com>
2024-09-21 15:45:21 +0700

    add class-validator

--- src/constants/responseMessage.ts
+++ src/constants/responseMessage.ts
@@ -1,5 +1,5 @@
 export const ResponseMessages = {
     DUPPLICATE_USERNAME: 'username has been taken',
     WEAK_PASSWORD:'Password must have at least 6 characters, 1 upper case, 1 lower case and 1 number',
-    
+    ABNORMAL_USERNAME_LENGTH:'Username must be from 6 to 20 characters'
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,22 @@
+import { IsString, IsStrongPassword, MaxLength, MinLength } from "class-validator"
+import { ResponseMessages } from "src/constants/responseMessage"
+
 export class CreateUserDto {
+    @IsString()
+    @MinLength(6,{message: ResponseMessages.ABNORMAL_USERNAME_LENGTH})
+    @MaxLength(20,{message: ResponseMessages.ABNORMAL_USERNAME_LENGTH})
     username: string
+
+    @IsString()
+    @IsStrongPassword({
+        minLength: 6,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 0
+    }, {message: ResponseMessages.WEAK_PASSWORD})
     password: string
+
     name?: string
     phone?: string
     address?: string
