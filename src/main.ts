1bf041bf89cf3ddd142b7b6278851b562917cfc3
Phi <voduchoangphi@gmail.com>
2024-09-22 14:00:57 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    allowedHeaders: JSON.parse(process.env.CORS_ALLOWED_HEADERS),
+    origin: JSON.parse(process.env.CORS_ALLOWED_ORIGINS),
+    methods: JSON.parse(process.env.CORS_ALLOWED_METHODS)
+  })
   await app.listen(3000);
 }
 bootstrap();
